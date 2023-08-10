e12d2d4acbfbb7476d8272a41391b35645763b4f
slarkin <slarkin@blackberry.com>
2023-08-10 15:43:05 -0400

    changes

--- server/alpaca/app/main.py
+++ server/alpaca/app/main.py
@@ -2,5 +2,15 @@ from fastapi import FastAPI
 
 from .routers import routes
 
+
 app = FastAPI()
 app.include_router(routes.router)
+
+
+
+COGNITO_USER_CLIENT_ID=
+COGNITO_REGION_NAME=
+USER_POOL_ID=
+COGNITO_JWKS_PATH=
+AWS_ACCESS_KEY_ID=
+AWS_SECRET_ACCESS_KEY=

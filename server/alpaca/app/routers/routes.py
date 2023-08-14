aed23c555b8c9454b25a0c5fe673dbd48a8c452a
slarkin <slarkin@blackberry.com>
2023-08-14 16:17:56 -0400

    utils update

--- server/alpaca/app/routers/routes.py
+++ server/alpaca/app/routers/routes.py
@@ -5,20 +5,23 @@ from ..services import crud
 
 router = APIRouter()
 
-# @router.get("/")
-# async def root():
-#     return {"message": "Server is running"}
+@router.get("/")
+async def root():
+    return {"message": "Server is running"}
 
 # User signs up for the platform
-@router.post("/")
+
+@router.post("/signup")
 async def create_user(user: schemas.User):
     username = user.email
     password = user.password
+    return [username, password]
     signup_result = crud.cognito_signup(username, password)
+     
     return signup_result
 
 # User logs into the platform
-@router.post("/platform/login")
+@router.post("/login")
 async def login_user(user: schemas.User):
     username = user.email
     password = user.password
--- server/alpaca/app/utils/utils.py
+++ server/alpaca/app/utils/utils.py
@@ -1,7 +1,29 @@
 import cognitojwt
+import os 
+
+from fastapi import HTTPException
 
 class CognitoResponse(object):
     def __init__(self, access_token, refresh_token, cognito_user_id=None):
         self.access_token = access_token
         self.refresh_token = refresh_token
         self.cognito_user_id = cognito_user_id
+
+def authenticate_token(access_token: str):
+    REGION = os.environ.get('COGNITO_REGION_NAME')
+    USERPOOL_ID = os.environ.get('USER_POOL_ID')
+    APP_CLIENT_ID = os.environ.get('COGNITO_USER_CLIENT_ID')
+    # Attempt to decode the access token
+    try:
+        # Can get user properties from these claims
+        verified_claims: dict = cognitojwt.decode(
+            access_token,
+            REGION,
+            USERPOOL_ID,
+            app_client_id=APP_CLIENT_ID 
+        )
+    except:
+        raise HTTPException(
+            status_code=401,
+            detail="User is not authorized to access this resource. Check access token."
+        )
